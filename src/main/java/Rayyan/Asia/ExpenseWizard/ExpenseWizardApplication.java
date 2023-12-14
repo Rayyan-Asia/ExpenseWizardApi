5c7e1453f7672c70d9e875e82fdacb678194b95c
Rayyan-Asia <RayyanTawfieg@Gmail.com>
2023-12-14 23:53:36 +0200

    First Commit

---
+++ src/main/java/Rayyan/Asia/ExpenseWizard/ExpenseWizardApplication.java
@@ -0,0 +1,13 @@
+package Rayyan.Asia.ExpenseWizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Rayyan/Asia/ExpenseWizard/ExpenseWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package Rayyan.Asia.ExpenseWizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
